cd7324d128d492dd613cafe767f24ca59c11f6a2
Michael Riksman <mriksman@hotmail.com>
2024-12-07 14:49:07 +0000

    reboot to previous partition

--- main/main.c
+++ main/main.c
@@ -6,6 +6,7 @@
 #include "freertos/task.h"
 
 #include "esp_partition.h"
+#include "esp_ota_ops.h"
 #include "esp_log.h"
 #include "esp_flash.h"
 #include "esp_system.h"
@@ -46,28 +47,8 @@ static const char* get_subtype_str(esp_partition_subtype_t subtype)
     }
 }
 
-void app_main(void)
-{
-    esp_log_level_set("*", ESP_LOG_DEBUG);         
 
-    esp_partition_iterator_t it;
-
-    ESP_LOGI(TAG, "Iterating through partitions...");
-    it = esp_partition_find(ESP_PARTITION_TYPE_ANY, ESP_PARTITION_SUBTYPE_ANY, NULL);
-
-    // Loop through all matching partitions, in this case, all with the type 'data' until partition with desired
-    // label is found. Verify if its the same instance as the one found before.
-    for (; it != NULL; it = esp_partition_next(it)) {
-        const esp_partition_t *part = esp_partition_get(it);
-        ESP_LOGI(TAG, "\tfound partition with type %s, subtype %s, label '%s' at offset 0x%" PRIx32 " with size %" PRIu32 "KB",
-            get_type_str(part->type), get_subtype_str(part->subtype), part->label, part->address, part->size / 1024);
-    }
-
-    // Release the partition iterator to release memory allocated for it
-    esp_partition_iterator_release(it);
-
-    
-    
+void update_partition () {    
     const esp_partition_t *homekit_partition = esp_partition_find_first(ESP_PARTITION_TYPE_ANY, ESP_PARTITION_SUBTYPE_ANY, "homekit");
 
     if (homekit_partition) {
@@ -105,8 +86,51 @@ void app_main(void)
     }
     else {
         ESP_LOGW(TAG, "No partition named 'homekit' found, abort");
+        return;
     }
 
+}
+
+
+void app_main(void)
+{
+    esp_log_level_set("*", ESP_LOG_DEBUG);         
+
+    esp_partition_iterator_t it;
+
+    ESP_LOGI(TAG, "Iterating through partitions...");
+    it = esp_partition_find(ESP_PARTITION_TYPE_ANY, ESP_PARTITION_SUBTYPE_ANY, NULL);
+
+    // Loop through all matching partitions, in this case, all with the type 'data' until partition with desired
+    // label is found. Verify if its the same instance as the one found before.
+    for (; it != NULL; it = esp_partition_next(it)) {
+        const esp_partition_t *part = esp_partition_get(it);
+        ESP_LOGI(TAG, "\tfound partition with type %s, subtype %s, label '%s' at offset 0x%" PRIx32 " with size %" PRIu32 "KB",
+            get_type_str(part->type), get_subtype_str(part->subtype), part->label, part->address, part->size / 1024);
+    }
+
+    // Release the partition iterator to release memory allocated for it
+    esp_partition_iterator_release(it);
+
+    update_partition();
+ 
+    const esp_partition_t *running = esp_ota_get_running_partition();
+    ESP_LOGI(TAG, "Running OTA partition subtype %s )", get_subtype_str(running->subtype));
+
+    const esp_partition_t *next_boot_partition = NULL;
+    next_boot_partition = esp_ota_get_next_update_partition(NULL);
+
+    if (next_boot_partition) {
+        esp_err_t err = esp_ota_set_boot_partition(next_boot_partition);
+        if (err == ESP_OK) {
+            ESP_LOGI(TAG, "Next OTA boot partition successfully set");
+        } else {
+            ESP_LOGE(TAG, "Failed to set next OTA boot partition: %s", esp_err_to_name(err));
+        }
+    }
+
+    const esp_partition_t *boot_partition = esp_ota_get_boot_partition();
+    ESP_LOGI(TAG, "Next OTA boot partition subtype %s )", get_subtype_str(boot_partition->subtype));
 
 
 }
